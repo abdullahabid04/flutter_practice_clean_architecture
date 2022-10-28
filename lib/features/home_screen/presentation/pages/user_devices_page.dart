adb9473fd8bc7d154c11d337cb672b926a1d4826
pyUserAbdullah <abdrps2004@gmail.com>
2022-10-28 10:58:45 -0700

    started device page

---
+++ lib/features/home_screen/presentation/pages/user_devices_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserDeviceScreen extends StatefulWidget {
+  const UserDeviceScreen({Key? key}) : super(key: key);
+
+  @override
+  State<UserDeviceScreen> createState() => _UserDeviceScreenState();
+}
+
+class _UserDeviceScreenState extends State<UserDeviceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
