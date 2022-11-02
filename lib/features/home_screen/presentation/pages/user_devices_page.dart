b3d663120ff2e6fae0fd3bd3ec5183deafb90d62
pyUserAbdullah <abdrps2004@gmail.com>
2022-11-02 08:00:50 -0700

    completed user devices feature for getting all user devices just testing remaining after testing next target would be share delete rename and power

--- lib/features/home_screen/presentation/pages/user_devices_page.dart
+++ lib/features/home_screen/presentation/pages/user_devices_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:practice_clean_architecture/dependancy_injection.dart';
+import 'package:practice_clean_architecture/features/home_screen/presentation/widgets/device_widget.dart';
+import 'package:practice_clean_architecture/utils/show_progress.dart';
+import '../bloc/bloc.dart';
 
 class UserDeviceScreen extends StatefulWidget {
   const UserDeviceScreen({Key? key}) : super(key: key);
@@ -10,6 +16,39 @@ class UserDeviceScreen extends StatefulWidget {
 class _UserDeviceScreenState extends State<UserDeviceScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: _buildUserDevicesPage(context),
+    );
+  }
+
+  BlocProvider<UserDevicesBloc> _buildUserDevicesPage(BuildContext context) {
+    return BlocProvider(
+      create: (_) => getItServiceLocator<UserDevicesBloc>(),
+      child: BlocConsumer<UserDevicesBloc, UserDevicesState>(
+        listener: (context, state) {
+          if (state is UserDevicesInitialState) {
+            BlocProvider.of<UserDevicesBloc>(context).add(
+              GetAllUserDevicesEvent(),
+            );
+          }
+        },
+        builder: (context, state) {
+          if (state is UserDevicesLoadingState) {
+            return ShowProgress();
+          } else if (state is UserDevicesNotLoadedState) {
+            return Container();
+          } else if (state is UserDevicesLoadedState) {
+            return DeviceWidget(
+              userDevices: state.userDevices,
+            );
+          } else if (state is UserDevicesErrorState) {
+            return Container();
+          } else {
+            return Container();
+          }
+        },
+      ),
+    );
   }
 }
